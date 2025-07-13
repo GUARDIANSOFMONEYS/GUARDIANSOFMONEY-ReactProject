f324e486d043346ce975ff7393da17848f0de784
aysunbekdemir <aysunkoc9@hotmail.com>
2025-07-13 18:40:42 +0300

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,9 @@
-
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import Logo from "../assets/TabletLogo.png";
-import ExitIcon from "../assets/exit1.svg";
-import './Header.css'; 
+import Logo from "../../assest/logo.png";
+import ExitIcon from "../../assest/exit1.png";
+import "./Header.css";
 
 
 
